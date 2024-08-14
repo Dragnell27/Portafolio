95de86a5956072df8d694b10a70c753a811e4b76
Dragnell27 <jaduvase27@gmail.com>
2024-08-13 20:24:32 -0500

    Se cambia botón para descargar cv por un icono.

--- src/components/slider.jsx
+++ src/components/slider.jsx
@@ -6,20 +6,24 @@ export const Slider = ({ }) => {
                 <div className='img-header'>
                     <div className='img-content'>
                         <figure className='avatar'>
-                            <img src={avatar} alt='Avatar de Jaider Valencia'/>
+                            <img src={avatar} alt='Avatar de Jaider Valencia' />
                         </figure>
                         <div className='info'>
                             <h1 className='name'>Jaider Valencia</h1>
                             <h2 className='occupation'>Desarrollador Full Stack</h2>
                             <div className='professional-links'>
-                                <a className='cv' href='../documents/cv.pdf' target='_blank'>CV</a>
+                                <a className='links' href='../documents/CV_JAIDER_VALENCIA.pdf' target='_blank'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-text-fill" viewBox="0 0 16 16">
+                                        <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0M9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1M4.5 9a.5.5 0 0 1 0-1h7a.5.5 0 0 1 0 1zM4 10.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 0 1z" />
+                                    </svg>
+                                </a>
                                 <a className='links' href='https://github.com/Dragnell27' target='_blank'>
-                                <svg
-                                    xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor'
-                                    className='bi bi-github' viewBox='0 0 16 16'>
-                                    <path
-                                        d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8' />
-                                </svg>
+                                    <svg
+                                        xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor'
+                                        className='bi bi-github' viewBox='0 0 16 16'>
+                                        <path
+                                            d='M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8' />
+                                    </svg>
                                 </a>
                                 <a className='links' href='https://www.linkedin.com/in/jvalencias/' target='_blank'>
                                     <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 16 16'>
